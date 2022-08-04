47dd2d33023dbdeddad8a3cc73ffdc912208cc86
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-04 14:19:20 +0500

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -2,11 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DivisionByZero {
     public static void main(String[] args){
-        int number =10;
-        try{
-            System.out.println(number / 0);
-        } catch (Exception a){
-            System.out.println(a.getMessage());
-        }
+        float number = 10;
+        System.out.println(number/0);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,12 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        if (number < 0){
-            System.out.println(false);
-         } else if (number == 0) {
-            System.out.println(0);
-        } else{
-            System.out.println(true);
-        }
+        System.out.println(number > 0);
     }
 }
