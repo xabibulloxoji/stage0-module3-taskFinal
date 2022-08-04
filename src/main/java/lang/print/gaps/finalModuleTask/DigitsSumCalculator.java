5adee6ec0f9e47a49df1567ed030b7c4708f84c5
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-04 17:25:18 +0500

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,12 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
-        int i = number / 1000;
-        int i1 = number % 1000;
-        int i2 = i1 / 100;
-        int i3 = i1 % 100;
-        int i4 = i3 / 10;
-        int i5 = i3 % 10;
-        System.out.println(i + i2 + i4+ i5);
+        int sum=0;
+        while (number!=0){
+            sum =sum +number%10;
+            number/=10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class DivisionByZero {
     public static void main(String[] args){
         float number = 10;
-        if (number != 0) System.out.println(number/0);
+        System.out.println(number/0);
 
     }
 }
